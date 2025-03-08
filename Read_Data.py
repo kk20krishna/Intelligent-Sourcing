3ef39dacd953c86ba1b2051473922080d727e675
Krishna Kumar S <64156067+kk20krishna@users.noreply.github.com>
2025-03-08 07:44:46 +0000

    commit

--- Read_Data.py
+++ Read_Data.py
@@ -10,12 +10,14 @@ sheets = ['Weightage', 'Priority Data', 'Warehouse Data', 'Order Data', 'Cost Da
 
 weightage_df = pd.read_excel(filepath, sheet_name='Weightage')
 weightage_dict = dict(zip(weightage_df['Variable'], weightage_df['Weightage']))
-Weightage_Cost = weightage_dict["Cost"]
-Weightage_Priority = weightage_dict["Priority"]
-Weightage_distance = weightage_dict["Distance"]
-Weightage_days = weightage_dict["Days"]
+#Weightage_Cost = weightage_dict["Cost"]
+#Weightage_Priority = weightage_dict["Priority"]
+#Weightage_distance = weightage_dict["Distance"]
+#Weightage_days = weightage_dict["Days"]
 
 priority_df = pd.read_excel(filepath, sheet_name='Priority Data')
 warehouse_df = pd.read_excel(filepath, sheet_name='Warehouse Data')
-
-print(warehouse_df)
+order_df = pd.read_excel(filepath, sheet_name='Order Data')
+cost_df = pd.read_excel(filepath, sheet_name='Cost Data')
+Distance_df = pd.read_excel(filepath, sheet_name='Distance Data')
+Days_df = pd.read_excel(filepath, sheet_name='Days Data')
